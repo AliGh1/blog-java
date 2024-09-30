8889996edeb6504dee987a1b9d210237d48106cc
ali <ali77erfagh99@gmail.com>
2024-09-30 16:18:17 +0330

    refactor CustomErrorResponse

--- src/main/java/org/example/blog/exception/CustomErrorResponse.java
+++ src/main/java/org/example/blog/exception/CustomErrorResponse.java
@@ -1,20 +1,12 @@
 package org.example.blog.exception;
 
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Getter
-public class CustomErrorResponse {
-    private final int status;
-    private final String message;
-    private final String timestamp;
 
-
-    public CustomErrorResponse(int statusCode, String message) {
-        this.status = statusCode;
-        this.message = message;
-        this.timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+public record CustomErrorResponse(int status, String message, String timestamp) {
+    public CustomErrorResponse(int status, String message) {
+        this(status, message, LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
     }
+
 }
